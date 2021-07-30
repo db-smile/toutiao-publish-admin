5c31445bf3f6c92b467612c2507a9a13395c1a6f
db-smile <1098220204@qq.com>
2021-07-30 18:06:53 +0800

    init project 调整目录结构

--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import './styles/index.less'
 
 Vue.config.productionTip = false
+
+// 创建vue根实例
+// 把router配置到根实例中
 
 new Vue({
   router,
